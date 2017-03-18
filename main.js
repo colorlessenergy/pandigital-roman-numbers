e7f898774b87dc10e6ba08e8fd705908342c65d8
colorlessenergy <brianmunozdev@gmail.com>
2017-03-17 18:50:53 -0700

    displayed all pandigital roman numbers

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-// I = 1
-// V = 5
-// X = 10
-// L = 50
-// C = 100 check
-// D = 500
-// M = 1000 check
-// check if the number is  pandigital
-var number = prompt("check if number is a pandigital");
-var realNum = parseInt(number).toString().split("").map(Number)
+var pandigital = document.getElementById("pandigital");
 
-  if (realNum[0] === 1) {
-    for (var i = 1; i < realNum.length; i++) {
-      if (realNum[i] === 4 || realNum [i] === 6) {
-        alert(realNum.join("") + " is a pandigital roman number");
+for (var j = 0; j < 2000; j++) {
+var realNum = parseInt(j).toString().split("").map(Number);
+if (realNum[0] === 1 && realNum.length === 4) {
+  for (var i = 1; i < realNum.length; i++) {
+    if (realNum[i] === 4 || realNum[i] === 6) {
+      if (i === 2) {
+        if (realNum[i-1] === 4 || realNum[i-1] === 6) {
+          if (realNum[i+1] == 4 || realNum[i+1] === 6) {
+            var createLi = document.createElement("li");
+            createLi.innerHTML = realNum.join("");
+            pandigital.appendChild(createLi);
+          }
+        }
       }
     }
-  } else {
-    alert(realNum + " is not a pandigital roman number")
   }
+}
+}
